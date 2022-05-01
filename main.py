4d80594e74c95ce8630b229329e8985fc4326c8c
seneca86 <96617908+seneca86@users.noreply.github.com>
2022-05-01 11:55:24 +0200

    add

--- main.py
+++ main.py
@@ -79,3 +79,46 @@ plt.plot(test.date, preds, label="autoarima", color="blue")
 plt.legend()
 plt.savefig(directory + "/autoarima")
 # %%
+unemp_raw = pd.read_csv(".lesson/assets/fred_unemployment.csv", sep=",")
+unemp = unemp_raw.rename(
+    {
+        "DATE": "date",
+        "UNRATE": "rate",
+    },
+    axis="columns",
+).assign(
+    date=lambda x: pd.to_datetime(x.date, infer_datetime_format=True)
+)
+# %%
+var = 'rate'
+plt.plot(unemp.date, unemp[var], label="actuals")
+plt.legend()
+plt.savefig(directory + "/actuals_unemp")
+plt.clf()
+# %%
+plot_pacf(unemp[var], lags=20)
+plt.savefig(directory + "/pacf_actuals_unemp")
+pacf_values = pacf(unemp[var])
+print(pacf_values)
+# %%
+split = 850
+train = unemp.iloc[0:split, :]
+test = unemp.iloc[split:, :]
+# %%
+with StepwiseContext(max_dur=15):
+    model = pm.auto_arima(
+        train[var],
+        stepwise=True,
+        error_action="ignore",
+        seasonal=True,
+    )
+# %%
+print(f"model.summary()")
+preds, conf_int = model.predict(n_periods=unemp.shape[0] - split, return_conf_int=True)
+
+# %%
+plt.plot(train.date, train[var], label="actuals", color="black")
+plt.plot(test.date, preds, label="autoarima", color="blue")
+plt.legend()
+plt.savefig(directory + "/autoarima_unemp")
+# %%
